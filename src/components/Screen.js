2aac408bf1393850ef2a509b19ecbdf5aadd38e1
Bryan Nelson <nelsonbm@coolsite.biz>
2019-05-30 22:14:10 -0700

    fixed some test cases

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Screen(props) {
     return (
-        <div className="screenContainer">
-            <div className="screen">
-                <span className="screenText">{props.screenText}</span><br />
-                <span className="screenTextBig">{props.screenTextBig}</span>
+        <div className="screenContainer" >
+            <div className="screen" >
+                <span className="screenText" id={props.id}>{props.screenText}</span><br />
+                <span className="screenTextBig" >{props.screenTextBig}</span>
                 
             </div>
         </div>
